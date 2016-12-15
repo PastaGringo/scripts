c00eb824ba5a98f3c71b730e00c21f4eb2bed936
PastaGringo <cestdelafoudre@gmail.com>
2016-12-15 14:27:33 +0100

    Create check_mysql.sh

---
+++ check_mysql.sh
@@ -0,0 +1,22 @@
+echo "Please choose a password for the FreshRSS mysql user."
+read -s -p "Password: " 'nextpass'
+#Check for existing mysql and install if not found
+if [[ -n $inst ]]; then
+  echo -n -e "Existing mysql server detected!\n"
+  echo -n -e "Please enter mysql root password so that installation may continue:\n"
+  read -s -p "Password: " 'password'
+  echo -e "Please wait while FreshRSS is installed ... "
+else
+  echo -n -e "No mysql server found! Setup will install. \n"
+  echo -n -e "Please enter a mysql root password \n"
+  while [ -z "$password" ]; do
+    read -s -p "Password: " 'pass1'
+    echo
+    read -s -p "Re-enter password to verify: " 'pass2'
+    if [ $pass1 = $pass2 ]; then
+       password=$pass1
+    else
+       echo
+       echo "Passwords do not match"
+    fi
+  done
