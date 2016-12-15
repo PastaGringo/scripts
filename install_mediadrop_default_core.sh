d97e2d88f814fb1c44721fdccd722c6f6e549362
PastaGringo <cestdelafoudre@gmail.com>
2016-12-15 14:50:36 +0100

    Update install_mediadrop_default_core.sh

--- install_mediadrop_default_core.sh
+++ install_mediadrop_default_core.sh
@@ -1,67 +1,17 @@
-echo "Please choose a password for the FreshRSS mysql user."
-read -s -p "Password: " 'freshpass'
-#Check for existing mysql and install if not found
-if [[ -n $inst ]]; then
-  echo -n -e "Existing mysql server detected!\n"
-  echo -n -e "Please enter mysql root password so that installation may continue:\n"
-  read -s -p "Password: " 'password'
-  echo -e "Please wait while FreshRSS is installed ... "
-
-else
-  echo -n -e "No mysql server found! Setup will install. \n"
-  echo -n -e "Please enter a mysql root password \n"
-  while [ -z "$password" ]; do
-    read -s -p "Password: " 'pass1'
-    echo
-    read -s -p "Re-enter password to verify: " 'pass2'
-    if [ $pass1 = $pass2 ]; then
-       password=$pass1
-    else
-       echo
-       echo "Passwords do not match"
-    fi
-  done
-  echo -e "Please wait while FreshRSS is installed ... "
-  sudo apt-get -y install mariadb-server 
-  mysqladmin -u root password ${password}
-fi
-
-# If you use an Apache Web server (otherwise you need another Web server)
-sudo a2enmod headers expires rewrite ssl    #Apache modules
-
-# For Ubuntu >= 16.04, Debian >= 9 Stretch
-sudo apt install php php-curl php-gmp php-intl php-mbstring php-sqlite3 php-xml php-zip git -y
-sudo apt install libapache2-mod-php -y #For Apache
-
-# Restart Web server
-sudo service apache2 restart
-
-# For FreshRSS itself (git is optional if you manually download the installation files)
-cd /srv
-sudo git clone https://github.com/FreshRSS/FreshRSS.git
-cd FreshRSS
-
-# Set the rights so that your Web server can access the files
-sudo chown -R :www-data . && sudo chmod -R g+r . && sudo chmod -R g+w ./data/
-
-cat > /etc/apache2/sites-enabled/freshrss.conf <<EOF
-Alias /freshrss "/srv/FreshRSS/"
-<Directory /srv/FreshRSS/>
- Options +FollowSymlinks
- AllowOverride All
- Require all granted
-</Directory>
-EOF
-
-mysql --user="root" --password="$password" --execute="CREATE DATABASE freshrss;"
-mysql --user="root" --password="$password" --execute="CREATE USER freshrss@localhost IDENTIFIED BY '$freshpass';"
-mysql --user="root" --password="$password" --execute="GRANT ALL PRIVILEGES ON freshrss.* TO freshrss@localhost;"
-mysql --user="root" --password="$password" --execute="FLUSH PRIVILEGES;"
-
-service apache2 reload
-touch /install/.freshrss.lock
-
-echo -e "Visit https://${ip}/freshrss to finish installation."
-echo -e "Database user: freshrss"
-echo -e "Database password: ${freshpass}"
-echo -e "Database name: freshrss"
+-!#/bin/bash
+ -sqlroot=root
+ -sqlrootpwd=password
+ -apt-get update
+ -apt-get -y install libjpeg-dev zlib1g-dev libfreetype6-dev libmysqlclient-dev python-dev python-setuptools python-virtualenv git build-essential mysql-server mysql-client
+ -debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+ -debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+ -mysql -u $sqlroot -p$sqlrootpwd -Bse "create database mediadrop_db;grant usage on mediadrop_db.* to mediadrop_user@localhost identified by 'mysecretpassword';grant all privileges on mediadrop_db.* to mediadrop_user@localhost;exit;"
+ -virtualenv --no-site-packages MediaDropEnv
+ -source MediaDropEnv/bin/activate
+ -git clone git://github.com/mediadrop/mediadrop.git mediadrop-git
+ -cd mediadrop-git
+ -python setup.py develop
+ -wget https://raw.githubusercontent.com/PastaGringo/scripts/master/deployment.ini
+ -paster setup-app deployment.ini
+ -mysql -u $sqlroot -p$sqlrootpwd "mediadrop_db" < "setup_triggers.sql"
+ -paster serve --reload deployment.ini
