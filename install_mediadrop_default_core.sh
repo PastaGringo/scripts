0415089429bce89cab2f2202502f2e9b28026a3a
PastaGringo <cestdelafoudre@gmail.com>
2016-12-15 14:38:05 +0100

    Update install_mediadrop_default_core.sh

--- install_mediadrop_default_core.sh
+++ install_mediadrop_default_core.sh
@@ -1,17 +1,27 @@
-!#/bin/bash
-sqlroot=root
-sqlrootpwd=password
-apt-get update
-apt-get -y install libjpeg-dev zlib1g-dev libfreetype6-dev libmysqlclient-dev python-dev python-setuptools python-virtualenv git build-essential mysql-server mysql-client
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
-mysql -u $sqlroot -p$sqlrootpwd -Bse "create database mediadrop_db;grant usage on mediadrop_db.* to mediadrop_user@localhost identified by 'mysecretpassword';grant all privileges on mediadrop_db.* to mediadrop_user@localhost;exit;"
-virtualenv --no-site-packages MediaDropEnv
-source MediaDropEnv/bin/activate
-git clone git://github.com/mediadrop/mediadrop.git mediadrop-git
-cd mediadrop-git
-python setup.py develop
-wget https://raw.githubusercontent.com/PastaGringo/scripts/master/deployment.ini
-paster setup-app deployment.ini
-mysql -u $sqlroot -p$sqlrootpwd "mediadrop_db" < "setup_triggers.sql"
-paster serve --reload deployment.ini
+echo "Please choose a password for the FreshRSS mysql user."
+read -s -p "Password: " 'freshpass'
+#Check for existing mysql and install if not found
+if [[ -n $inst ]]; then
+  echo -n -e "Existing mysql server detected!\n"
+  echo -n -e "Please enter mysql root password so that installation may continue:\n"
+  read -s -p "Password: " 'password'
+  echo -e "Please wait while FreshRSS is installed ... "
+
+else
+  echo -n -e "No mysql server found! Setup will install. \n"
+  echo -n -e "Please enter a mysql root password \n"
+  while [ -z "$password" ]; do
+    read -s -p "Password: " 'pass1'
+    echo
+    read -s -p "Re-enter password to verify: " 'pass2'
+    if [ $pass1 = $pass2 ]; then
+       password=$pass1
+    else
+       echo
+       echo "Passwords do not match"
+    fi
+  done
+  echo -e "Please wait while FreshRSS is installed ... "
+  sudo apt-get -y install mariadb-server > /dev/null 2>&1
+  mysqladmin -u root password ${password}
+fi
