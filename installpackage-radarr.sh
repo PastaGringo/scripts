1ed955c608c1dc730aacc4d79831652a1982497d
PastaGringo <cestdelafoudre@gmail.com>
2017-01-23 14:00:53 +0100

    Update installpackage-radarr.sh

--- installpackage-radarr.sh
+++ installpackage-radarr.sh
@@ -6,19 +6,14 @@ echo
 MASTER=$(cat /srv/rutorrent/home/db/master.txt)
 ip=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 
-function _Radarr_dependencies() {
 apt update
 apt-get install -y libmono-cil-dev curl mediainfo
-}
 
-function _Radarr_install() {
 cd /opt
 wget https://github.com/Radarr/Radarr/releases/download/v0.2.0.210/Radarr.develop.0.2.0.210.linux.tar.gz
 tar -xvzf /opt/Radarr.develop.0.2.0.99.linux.tar.gz
 rm -rf /opt/Radarr.develop.0.2.0.99.linux.tar.gz
-}
 
-function _Radarr_setup() {
 cat > /etc/systemd/system/radarr.service <<EOF
 [Unit]
 Description=Radarr Daemon
@@ -37,17 +32,11 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-}
 
-function _Radarr_enable() {
+systemctl daemon-reload
 systemctl enable radarr.service
-service radarr start
+systemctl start radarr.service
+
 echo "Radarr is installed !"
 echo "You can access it at  : $ip:7878"
 echo "Have fun!"
-}
-
-_Radarr_dependencies
-_Radarr_install
-_Radarr_setup
-_Radarr_enable
