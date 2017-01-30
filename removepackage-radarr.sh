8dda683f3896ed69558cbc8a3fc5772439f56923
PastaGringo <cestdelafoudre@gmail.com>
2017-01-30 16:34:03 +0100

    Update removepackage-radarr.sh

--- removepackage-radarr.sh
+++ removepackage-radarr.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+systemctl stop Radarr
 systemctl disable Radarr
 rm -rf /etc/systemd/system/radarr.service
 rm -rf /opt/Radarr
+rm -rf /etc/apache2/sites-enabled/radarr.conf
+echo "Radarr uninstalled!"
