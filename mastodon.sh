727d99c2091b36055bec9f5487754fcad63c54eb
PastaGringo <cestdelafoudre@gmail.com>
2017-04-06 23:05:09 +0200

    Update mastodon.sh

--- mastodon.sh
+++ mastodon.sh
@@ -2,10 +2,12 @@
 echo 
 echo mastodon.sh
 echo remove all sudo
+echo 
 echo go
+echo
 echo "deb http://httpredir.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
 apt update && apt full-upgrade -y
-apt-get install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file curl git
+apt-get install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file curl git -y
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
 apt install nodejs -y
 npm install -g yarn 
