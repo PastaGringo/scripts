8449c63e1b382fa1d75c89e2cf0b4017e3e965d9
PastaGringo <cestdelafoudre@gmail.com>
2017-04-07 14:41:28 +0200

    Update mastodon.sh

--- mastodon.sh
+++ mastodon.sh
@@ -13,10 +13,24 @@ apt install nodejs -y
 npm install -g yarn 
 apt install redis-server redis-tools -y
 apt-get install postgresql postgresql-contrib -y
-
+su - postgres -c "psql -c 'CREATE USER mastodon CREATEDB;'"  
+adduser --disabled-password --disabled-login mastodon
+apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev -y
 sudo -u mastodon bash << EOF
-psql <<EOF
-CREATE USER mastodon CREATEDB;
-\q
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+cd ~/.rbenv && src/configure && make -C src
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"' >> ~/.bash_profile
 EOF
+exit
+sudo -u mastodon bash << EOF
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo Installation de Ruby 2.3.1
+rbenv install 2.3.1
+cd ~
+git clone https://github.com/tootsuite/mastodon.git live
+cd live
+gem install bundler
+bundle install --deployment --without development test
+yarn install
 EOF
