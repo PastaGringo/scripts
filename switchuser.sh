8937981fa1eb840d6aa6481b89a7f9726e7575b6
PastaGringo <cestdelafoudre@gmail.com>
2017-04-07 12:04:02 +0200

    Update switchuser.sh

--- switchuser.sh
+++ switchuser.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+adduser --disabled-password --disabled-login mastodon
+adduser --disabled-password --disabled-login postgre
 whoami
-sudo -u someuser bash << EOF
+sudo -u mastodon bash << EOF
+echo "In"
+whoami
+EOF
+echo "Out"
+whoami
+sudo -u postgre bash << EOF
 echo "In"
 whoami
 EOF
