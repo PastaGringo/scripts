bf737879de5475c2a88a20eeeef2ad989c20b9ac
PastaGringo <cestdelafoudre@gmail.com>
2017-04-07 15:49:50 +0200

    Update mastodon.sh

--- mastodon.sh
+++ mastodon.sh
@@ -13,7 +13,7 @@ npm install -g yarn
 apt install redis-server redis-tools -y
 apt-get install postgresql postgresql-contrib -y
 su - postgres -c "psql -c 'CREATE USER mastodon CREATEDB;'"  
-adduser --disabled-password --disabled-login mastodon
+adduser --disabled-password --disabled-login --gecos "" mastodon
 apt install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev -y
 sudo -u mastodon bash << EOF
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
