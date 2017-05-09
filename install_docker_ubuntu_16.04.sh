0d632f0994ec5c43eeebc4901083a6e1ba0343c5
PastaGringo <cestdelafoudre@gmail.com>
2017-05-09 16:44:50 +0200

    Create install_docker_ubuntu_16.04.sh

---
+++ install_docker_ubuntu_16.04.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+apt-get install software-properties-common python-software-properties -y
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
+apt-get install docker-engine -y
+systemctl status docker
