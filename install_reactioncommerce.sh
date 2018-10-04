8c8ff46840463ad0c9bfbee5f310a1cfd4ae2e6b
PastaGringo <cestdelafoudre@gmail.com>
2018-10-04 17:07:05 +0200

    Update install_reactioncommerce.sh

--- install_reactioncommerce.sh
+++ install_reactioncommerce.sh
@@ -3,6 +3,7 @@ echo Installation...
 sudo apt-get update
 sudo apt-get install -y --no-install-recommends build-essential bzip2 curl ca-certificates git python npm nodejs
 curl https://install.meteor.com/ | sh
+sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g reaction-cli
 reaction init
 cd reaction
